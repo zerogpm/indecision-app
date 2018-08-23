fabad1f00dad502ef0f78425c40f210514b62969
zerogpm <zerogpm@gmail.com>
2018-08-22 21:48:31 -0700

    component

---
+++ src/playground/jsx-indecision.js
@@ -0,0 +1,55 @@
+console.log('App.js is running!');
+
+let app = {
+  title: 'Indecision App',
+  subtitle: 'This is some info',
+  options: []
+};
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
+  }
+};
+
+const onRemove = () => {
+  app.options = [];
+  render();
+};
+
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+};
+
+const appRoot = document.getElementById('app');
+const render = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {(app.subtitle) && <p>{app.subtitle}</p>}
+      <p>{ app.options.length > 0 ? 'Here are your options' : 'No options' }</p>
+      <p>{app.options.length}</p>
+      <button disabled={!Array.isArray(app.options) || !app.options.length} onClick={onMakeDecision}>What should I do?</button>
+      <button onClick={onRemove}>Remove All</button>
+      <ol>
+        {
+          app.options.map((option) => <li key={option}>your option: {option}</li>)
+        }
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option"/>
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+  ReactDOM.render(template, appRoot);
+};
+
+render();
